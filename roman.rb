01dca34415214f7170f9b7efc2f140e50ecf6cf8
Chai Phonbopit <phonbopit@gmail.com>
2014-11-18 16:19:25 +0700

    add test 2nd

--- roman.rb
+++ roman.rb
@@ -12,5 +12,9 @@ describe "roman" do
 	it "converts the number 1 to the string I" do
 		roman(1).must_equal "I"
 	end
+
+	it "converts the number 2 to the string II" do
+		roman(2).must_equal "II"
+	end
 end
 
